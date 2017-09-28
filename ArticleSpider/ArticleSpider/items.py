b44cb3b82fd7bbcbe1d6b5403f878c54d818b5fc
xushiming <twisted1995@163.com>
2017-09-29 01:06:32 +0800

    add lagou spider item

--- ArticleSpider/ArticleSpider/items.py
+++ ArticleSpider/ArticleSpider/items.py
@@ -172,3 +172,28 @@ class ZhihuAnswerItem(scrapy.Item):
         )
 
         return insert_sql, params
+
+
+class LagouJobItemLoader(ItemLoader):
+    # 自定义ItemLoader
+    default_output_processor = TakeFirst()
+
+
+class LagouJobItem(scrapy.Item):
+    # 拉勾网职位信息
+    title = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    salary = scrapy.Field()
+    job_city = scrapy.Field()
+    work_years = scrapy.Field()
+    degree_need = scrapy.Field()
+    job_type = scrapy.Field()
+    publish_time = scrapy.Field()
+    job_advantage = scrapy.Field()
+    job_desc = scrapy.Field()
+    job_addr = scrapy.Field()
+    company_name = scrapy.Field()
+    company_url = scrapy.Field()
+    tags = scrapy.Field()
+    crawl_time = scrapy.Field()
