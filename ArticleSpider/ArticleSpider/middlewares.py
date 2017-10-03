9c0cfc325690af2e33df361ffa6913d1740f8215
xushiming <twisted1995@163.com>
2017-10-03 20:36:26 +0800

    random user_agent

--- ArticleSpider/ArticleSpider/middlewares.py
+++ ArticleSpider/ArticleSpider/middlewares.py
@@ -6,6 +6,7 @@
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+from fake_useragent import UserAgent
 
 
 class ArticlespiderSpiderMiddleware(object):
@@ -58,4 +59,17 @@ class ArticlespiderSpiderMiddleware(object):
 
 class RandomUserAgentMiddleware(object):
     #  随机更换 user-agent
-    pass
+    def __init__(self, crawler):
+        super(RandomUserAgentMiddleware, self).__init__()
+        self.ua = UserAgent()
+        self.ua_type = crawler.settings.get("RANDOM_UA_TYPE", "random")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def process_request(self, request, spider):
+        def get_ua():
+            return getattr(self.ua, self.ua_type)
+
+        request.headers.setdefault('User-Agent', get_ua())
