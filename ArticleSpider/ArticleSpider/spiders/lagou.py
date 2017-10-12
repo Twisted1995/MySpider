7fc841395284de737d2db2d7b0a98e040d4eccb7
xushiming <twisted1995@163.com>
2017-10-12 13:25:34 +0800

    fixed pep8

--- ArticleSpider/ArticleSpider/spiders/lagou.py
+++ ArticleSpider/ArticleSpider/spiders/lagou.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from datetime import datetime
+import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from items import LagouJobItem, LagouJobItemLoader
