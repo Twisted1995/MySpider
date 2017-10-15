1139d7c7fe0285bf7a77f36721dc3a82be569ee6
Unknown <twisted1995@163.com>
2017-10-15 15:52:01 +0800

    fixed bugs

--- ArticleSpider/main.py
+++ ArticleSpider/main.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-
-from scrapy.cmdline import execute
+#  -*- coding: utf-8 -*-
 
 import sys
 import os
+from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
